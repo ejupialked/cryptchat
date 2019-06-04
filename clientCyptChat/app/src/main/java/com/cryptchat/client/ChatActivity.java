f014b2139560bd39b9726a486f0a4659f0a28826
Alked Ejupi <ae7g18@soton.ac.uk>
2019-06-04 17:04:34 +0100

    activity_chat design completed

--- clientCyptChat/app/src/main/java/com/cryptchat/client/ChatActivity.java
+++ clientCyptChat/app/src/main/java/com/cryptchat/client/ChatActivity.java
@@ -1,4 +1,12 @@
 package com.cryptchat.client;
 
-public class ChatActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ChatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
 }
