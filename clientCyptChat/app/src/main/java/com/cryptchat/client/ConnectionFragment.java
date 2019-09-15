1c661ea57ad5621c620584b0ca9a57420ac39971
Alked Ejupi <ae7g18@soton.ac.uk>
2019-09-15 22:14:14 +0200

    add fragments

---
+++ clientCyptChat/app/src/main/java/com/cryptchat/client/ConnectionFragment.java
@@ -0,0 +1,28 @@
+package com.cryptchat.client;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ConnectionFragment extends DialogFragment {
+
+
+
+    public static ConnectionFragment getInstance(){
+        return new ConnectionFragment();
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_connection, container, false);
+    }
+}
---
+++ clientCyptChat/app/src/main/java/com/cryptchat/client/LoadingKeyFragment.java
@@ -0,0 +1,32 @@
+package com.cryptchat.client;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class LoadingKeyFragment extends DialogFragment {
+
+
+
+    public static LoadingKeyFragment getInstance(){
+        return new LoadingKeyFragment();
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_publickey, container, false);
+    }
+
+
+}
